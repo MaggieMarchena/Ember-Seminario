4909631482f1aa2c3c9045f8a7d1e07ad1ec6e67
Maggie Marchena <maguimarchena@gmail.com>
2018-11-12 16:33:45 -0300

    refactor a componente para búsquedas

--- app/routes/search.js
+++ app/routes/search.js
@@ -3,8 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   actions: {
     doSearch(searchTerm) {
-      this.transitionTo('search-results');
-      // console.log("searchTerm: " + searchTerm);
+      this.transitionTo('search-results', {queryParams: {term: searchTerm}});
     }
   }
 });
